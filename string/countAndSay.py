f34abcc24696c5f48faff623edb315b07c7c392a
liuxiaoxiong <17966083+Xiaoxiong-Liu@users.noreply.github.com>
2018-08-25 14:01:47 +0800

    Create countAndSay.py

---
+++ string/countAndSay.py
@@ -0,0 +1,60 @@
+"""
+数数并说
+报数序列是指一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 被读作  "one 1"  ("一个一") , 即 11。
+11 被读作 "two 1s" ("两个一"）, 即 21。
+21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+
+给定一个正整数 n ，输出报数序列的第 n 项。
+
+注意：整数顺序将表示为一个字符串。
+
+示例 1:
+
+输入: 1
+输出: "1"
+示例 2:
+
+输入: 4
+输出: "1211"
+
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/39/
+
+"""
+
+
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        
+        """
+
+        re = '1'
+        
+        for i in range(n-1):
+            re = self.getNext(re)
+        return re
+            
+    def getNext(self, string):
+        length = len(string)
+        result = ""
+        j = 0
+        i = 0
+        while i < length:
+            now = string[i]
+            j = 0
+            while i<length and string[i]==now:
+                j += 1
+                i += 1
+            result = result+str(j)+now        
+        return result
+            
+    # 2018.08.23 写的真好。
