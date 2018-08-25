8a4fee0943ec3784ee13cd080429225c0b9460bd
liuxiaoxiong <17966083+Xiaoxiong-Liu@users.noreply.github.com>
2018-08-25 14:12:39 +0800

    Create hasCycle.py

---
+++ linkedList/hasCycle.py
@@ -0,0 +1,35 @@
+"""
+环形链表
+给定一个链表，判断链表中是否有环。
+
+进阶：
+你能否不使用额外空间解决此题？
+
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/46/
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution():
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head
+        if fast == None or fast.next == None or fast.next.next == None:
+            return False
+        while fast != None and fast.next != None:
+            fast = fast.next
+            fast = fast.next
+            head = head.next
+            if fast == head:
+                return True
+            
+        return False
