275ca3ba1c18b72a690652d4b94c68e6f1bf659d
liuxiaoxiong <17966083+Xiaoxiong-Liu@users.noreply.github.com>
2018-08-25 14:02:25 +0800

    Create longestCommonPrefix.py

---
+++ string/longestCommonPrefix.py
@@ -0,0 +1,42 @@
+"""
+最长公共前缀
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z 。
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        
+        result = ""
+        s0 = strs[0]
+        for i in range(len(s0)):
+            s = s0[i]
+            for j in range(1,len(strs)):
+                if len(strs[j]) <= i or  strs[j][i] !=s:
+                    return result
+            result += s
+        return result
+            
