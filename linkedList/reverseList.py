aabd647f387532ab6327788f29ba761aa5a3da17
liuxiaoxiong <17966083+Xiaoxiong-Liu@users.noreply.github.com>
2018-08-25 14:09:41 +0800

    Create reverseList.py

---
+++ linkedList/reverseList.py
@@ -0,0 +1,42 @@
+"""
+反转链表
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/43/
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+            
+        if head.next != None:
+            h = self.reverseList(head.next)
+            # 这里可以改进
+            # ht = h
+            # while ht.next != None:
+            #     ht = ht.next
+            # ht.next = head                          # h的尾巴
+            head.next.next = head                     #这个写得就很好 上面获取的ht其实就是head.next    
+                        
+            head.next = None                        #之前一直忽略的问题
+            return h
