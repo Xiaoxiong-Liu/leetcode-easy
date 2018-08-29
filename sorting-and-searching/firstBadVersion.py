670eeba013e91382336ea77850e97874c7984e8b
liuxiaoxiong <17966083+Xiaoxiong-Liu@users.noreply.github.com>
2018-08-29 16:19:55 +0800

    第一个错误的版本
    更新了迭代版本。

--- sorting-and-searching/firstBadVersion.py
+++ sorting-and-searching/firstBadVersion.py
@@ -1,3 +1,26 @@
+"""
+第一个错误的版本
+你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+
+你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+
+示例:
+
+给定 n = 5，并且 version = 4 是第一个错误的版本。
+
+调用 isBadVersion(3) -> false
+调用 isBadVersion(5) -> true
+调用 isBadVersion(4) -> true
+
+所以，4 是第一个错误的版本。 
+
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/8/sorting-and-searching/53/
+
+"""
+
+
 # The isBadVersion API is already defined for you.
 # @param version, an integer
 # @return a bool
@@ -12,26 +35,35 @@ class Solution:
         """
         return self.bad(1,n)
         
-        
-    def bad(self,begin,end):
-            if begin>end:
-            return -1
-        if begin == end:
-            return begin
-            
+#   递归版本
+    def bad2(self,begin,end):
         b = isBadVersion(begin)
         e = isBadVersion(end)
         mid = (begin+end)//2
-        m = isBadVersion(mid)    
-
+        m = isBadVersion(mid)
+        
+        
+        if begin>end:
+            return -1
+        if begin == end:
+            return begin
         if begin == end - 1 and b==False and e == True:
             return end
-        
-        
-        
-        
-        
+                
         if m != False:
             return self.bad(begin,mid)
         else:
             return self.bad(mid,end)
+    
+#   迭代版本
+    def bad(self,begin,end):
+        while begin < end-1:
+            mid = (begin+end)//2
+            m = isBadVersion(mid)
+            if m:
+                end = mid
+            else:
+                begin = mid
+        if isBadVersion(begin) == True:
+            return begin
+        return end
