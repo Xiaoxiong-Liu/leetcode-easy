caf6403da6b26fd4099517062d6effbb8352ef9d
liuxiaoxiong <17966083+Xiaoxiong-Liu@users.noreply.github.com>
2018-09-25 16:50:24 +0800

    只出现一次的数字

--- array/singleNumber.py
+++ array/singleNumber.py
@@ -1,3 +1,25 @@
+“”“
+只出现一次的数字
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,1]
+输出: 1
+示例 2:
+
+输入: [4,1,2,1,2]
+输出: 4
+    
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/25/
+    
+“”“
+
+
 class Solution:
     def singleNumber(self, nums):
         """
@@ -12,10 +34,9 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)==1:
-            return nums[0]
         r = 0
         for i in nums:
             r ^= i
         return r
             
+# 2018.09.25 精简了一下代码。
